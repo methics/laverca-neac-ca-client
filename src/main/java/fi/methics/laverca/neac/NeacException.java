80f3901439125a3a8e163f29c4a58a687ea60684
Eemeli Miettinen <eemeli.miettinen@methics.fi>
2023-06-26 18:51:54 +0300

    Fixed build. Updated version. Added error code constants. (#3)

--- src/main/java/fi/methics/laverca/neac/NeacException.java
+++ src/main/java/fi/methics/laverca/neac/NeacException.java
@@ -10,53 +10,24 @@ public class NeacException extends RuntimeException {
     private NeacErrorResp error;
     private int httpcode = -1;
     
-    /**
-     * Create an exception that highlights that the {@link NeacCaClient#authLogin()}
-     * must be called first
-     * @return Exception
-     */
-    public static NeacException createNotLoggedInException() {
-        NeacException ex = new NeacException();
-        ex.error = new NeacErrorResp();
-        ex.error.code    = 400;
-        ex.error.message = "Client is not logged in";
-        return ex;
-    }
-    /**
-     * Create an exception that highlights that the request is missing a parameter
-     * @param param Name of missing parameter
-     * @return Exception
-     */
-    public static NeacException createMissingParamException(String param) {
-        NeacException ex = new NeacException();
-        ex.error = new NeacErrorResp();
-        ex.error.code    = 400;
-        ex.error.message = "Missing parameter " + param;
-        return ex;
-    }
-    
     public NeacException(Response response) {
         try {
             this.httpcode = response.code();
             this.error    = NeacErrorResp.fromResponse(response);
         } catch (Exception e) {
             this.error = new NeacErrorResp();
-            error.code    = 500;
+            error.code    = NeacErrorResp.SYSTEM_ERROR;
             error.message = "Failed to parse error:" + e.getMessage();
             e.printStackTrace();
         }
     }
     
     public NeacException(Exception e) {
-        this.error = new NeacErrorResp();
-        error.code    = 500;
+        this.error    = new NeacErrorResp();
+        error.code    = NeacErrorResp.SYSTEM_ERROR;
         error.message = "Request failed: " + e.getMessage();
     }
 
-    private NeacException() {
-        
-    }
-    
     /**
      * Get error JSON
      * @return error JSON if available
